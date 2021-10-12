6f2c78b947956038cc8cb710c6276ac290dd241f
Charph Dang <charph.dang@activenetwork.com>
2021-10-12 10:43:56 +0800

    chore: add prettier and husky lint-stage to automatic format files

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Eslint + Prettier
+
+
+
+## Prepare your environment
+
+1. Install package that you need
+2. `npx husky add .husky/pre-commit "npx lint-staged" `
+3. 
+
---
+++ webapp/src/app.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+class App extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+ReactDom.render(<App />, document.querySelector('#app'));
