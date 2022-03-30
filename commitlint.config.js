90cbee354a62851e98eb78192b5cf28f2adf9fb1
Charph <charph.dang@gmail.com>
2022-03-30 21:05:51 +0800

    chore: update commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'build', 'revert', 'chore', 'merge']
+    ]
+  }
+};
